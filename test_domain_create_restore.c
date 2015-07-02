fff1fee63fe75f3b565feca8de50fd5145bdecf7
Euan Harris <euan.harris@citrix.com>
2015-07-02 14:27:19 +0000

    test_domain_create_restore: Extract the core of the test
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- test_domain_create_restore.c
+++ test_domain_create_restore.c
@@ -117,51 +117,56 @@ teardown_suite(FILE *suspend_file)
     fclose(suspend_file);
 }
 
+
+bool
+execute(struct test *tc, uint32_t *domid, libxl_domain_config *dc, libxl_domain_restore_params *params, int count, FILE* suspend_file)
+{
+    struct event ev;
+    int rc;
+
+    printf("\n****** Will cancel after %d events ******\n", count);
+    do_domain_create_restore(tc, dc, domid, fileno(suspend_file), params);
+
+    if (wait_until_n(tc, EV_LIBXL_CALLBACK, count, &ev, 50)) {
+        /* The API call returned before we could cancel it.
+           It should have returned successfully.
+         */
+        verify_completed(tc, *domid, ev);
+        return false;
+    }
+
+    /*
+     * Calling cancel on a cancellable operation should not return an
+     * error, unless the operation happened to complete in the meantime.
+     */
+    rc = libxl_ao_cancel(tc->ctx, &tc->ao_how);
+    printf("libxl_ao_cancel returned %d\n", rc);
+    assert(rc == ERROR_NOTFOUND || rc == 0);
+    wait_for(tc, EV_LIBXL_CALLBACK, &ev);
+
+    verify_cancelled(tc, *domid, ev);
+    return true;
+}
+
 void *testcase(struct test *tc)
 {
     int count;
-
     FILE *suspend_file;
-    int suspend_fd;
-
     setup_suite(tc, &suspend_file);
-    suspend_fd = fileno(suspend_file);
 
-    /* test should wait for some number of fd_events, cancel, then
-       skip all fd_events and cancel on the next non-fd event, or at
-       least the next fd event on a different fd */
     for (count = 1; count < 100; count++) {
         libxl_domain_config dc;
         libxl_domain_restore_params params;
         uint32_t domid = -2;
-        struct event ev;
-        int rc;
+        bool cont;
 
         setup(tc, &dc, suspend_file, &params);
+        cont = execute(tc, &domid, &dc, &params, count, suspend_file);
+        teardown(tc, domid, &dc);
 
-        printf("\n****** Will cancel after %d events ******\n", count);
-	do_domain_create_restore(tc, &dc, &domid, suspend_fd, &params);
-
-        if (wait_until_n(tc, EV_LIBXL_CALLBACK, count, &ev, 50)) {
-            /* The API call returned before we could cancel it.
-               It should have returned successfully.
-             */
-            verify_completed(tc, domid, ev);
-            teardown(tc, domid, &dc);
+        if (!cont) {
             break;
         }
-
-        /*
-         * Calling cancel on a cancellable operation should not return an
-         * error, unless the operation happened to complete in the meantime.
-         */
-        rc = libxl_ao_cancel(tc->ctx, &tc->ao_how);
-        printf("libxl_ao_cancel returned %d\n", rc);
-        assert(rc == ERROR_NOTFOUND || rc == 0);
-        wait_for(tc, EV_LIBXL_CALLBACK, &ev);
-
-        verify_cancelled(tc, domid, ev);
-        teardown(tc, domid, &dc);
     }
 
     teardown_suite(suspend_file);
